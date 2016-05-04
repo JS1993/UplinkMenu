1b95caa41f5844c0a98311ef9a6fccdd76ed68f1
susu <28025920@qq.com>
2016-05-04 14:59:52 +0800

    完成让一个控制器管理两个tableView实现级联菜单

--- UplinkMenu/UplinkMenu/ViewController.m
+++ UplinkMenu/UplinkMenu/ViewController.m
@@ -7,37 +7,81 @@
 //
 
 #import "ViewController.h"
-#import "CategoryTableViewController.h"
-#import "SubcategoryTableViewController.h"
+#import "category.h"
 
-@interface ViewController ()
+
+@interface ViewController ()<UITableViewDelegate,UITableViewDataSource>
+@property (strong, nonatomic) IBOutlet UITableView *categoryTableView;
+@property (strong, nonatomic) IBOutlet UITableView *subcategoryTableView;
+
+@property(strong,nonatomic)NSArray* categories;
 
 @end
 
 @implementation ViewController
 
-- (void)viewDidLoad {
-    [super viewDidLoad];
-    
-    CategoryTableViewController* cateVC=[[CategoryTableViewController alloc]init];
-    
-    cateVC.view.frame=CGRectMake(0, 0, self.view.bounds.size.width/2, self.view.bounds.size.height);
-    
-    [self.view addSubview:cateVC.view];
-    
-    [self addChildViewController:cateVC];
-    
-    SubcategoryTableViewController* subcateVC=[[SubcategoryTableViewController alloc]init];
-    
-    cateVC.delegate=subcateVC;
-    
-    subcateVC.view.frame=CGRectMake(self.view.bounds.size.width/2, 0, self.view.bounds.size.width/2, self.view.bounds.size.height);
-    
-    [self.view addSubview:subcateVC.view];
-    
-    [self addChildViewController:subcateVC];
+-(NSArray *)categories{
+    if (_categories==nil) {
+        _categories=[category category];
+    }
+    return _categories;
 }
 
+-(void)viewDidLoad{
+    
+    self.categoryTableView.tableFooterView=[[UIView alloc]init];
+    
+    self.subcategoryTableView.tableFooterView=[[UIView alloc]init];
+    
+}
 
+#pragma mark-tableViewDataSource
 
+-(NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section{
+    if (tableView==self.categoryTableView) {
+        return self.categories.count;
+    }else{
+        category* cate=self.categories[self.categoryTableView.indexPathForSelectedRow.row];
+        
+        return cate.subcategories.count;
+    }
+}
+
+-(UITableViewCell*)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath{
+    
+    if (tableView==self.categoryTableView) {
+        
+        UITableViewCell* cell=[tableView dequeueReusableCellWithIdentifier:@"categoryCell" forIndexPath:indexPath];
+        
+        category* category=self.categories[indexPath.row];
+        
+        cell.imageView.image=[UIImage imageNamed:category.icon];
+        cell.imageView.highlightedImage=[UIImage imageNamed:category.highlighted_icon];
+        
+        cell.textLabel.text=category.name;
+        cell.textLabel.highlightedTextColor=[UIColor greenColor];
+        cell.textLabel.font=[UIFont systemFontOfSize:14];
+        
+        cell.accessoryType=UITableViewCellAccessoryDisclosureIndicator;
+        
+        return cell;
+    }else{
+        
+        UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"subcategoryCell" forIndexPath:indexPath];
+        
+        category* cate=self.categories[self.categoryTableView.indexPathForSelectedRow.row];
+        
+        cell.textLabel.text=cate.subcategories[indexPath.row];
+       
+        cell.textLabel.font=[UIFont systemFontOfSize:13];
+        
+        return cell;
+    }
+}
+
+-(void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath{
+    if (tableView==self.categoryTableView) {
+        [self.subcategoryTableView reloadData];
+    }
+}
 @end
